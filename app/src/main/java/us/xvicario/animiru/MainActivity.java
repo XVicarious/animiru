3ef993141e7700e78aadacdab4b196726f40ea6b
brianmaurer42 <bmaurer@xvicario.us>
2018-06-16 01:21:46 +0000

    Start work on displaying titles in a list

--- app/src/main/java/us/xvicario/animiru/MainActivity.java
+++ app/src/main/java/us/xvicario/animiru/MainActivity.java
@@ -1,14 +1,19 @@
 package us.xvicario.animiru;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.recyclerview.extensions.ListAdapter;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -18,8 +23,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -29,7 +38,10 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    @BindView(R.id.testText) TextView testText;
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.anime_search_result) RecyclerView animeResults;
+
+    ArrayList<Anime> animeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +58,10 @@ public class MainActivity extends AppCompatActivity
 
         ButterKnife.bind(this);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        animeList = new ArrayList<>();
+
+        animeResults.setAdapter(new AnimeListAdapter(animeList));
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -67,12 +82,6 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        try {
-            Object nines = new RetreiveAnimeTask().execute().get();
-            testText.setText(nines.toString());
-        } catch (Exception e) {
-            // todo: deal with this maybe
-        }
     }
 
     @Override
@@ -143,6 +152,43 @@ public class MainActivity extends AppCompatActivity
             }
             return nines.toString();
         }
+    }
+
+    class AnimeListAdapter extends RecyclerView.Adapter<AnimeListAdapter.AnimeViewHolder> {
+
+        ArrayList<Anime> anime;
+
+        AnimeListAdapter(ArrayList<Anime> anime) {
+            this.anime = anime;
+        }
+
+        @NonNull
+        @Override
+        public AnimeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new AnimeViewHolder(parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull AnimeViewHolder holder, int position) {
+            holder.animeName.setText(animeList.get(position).title);
+        }
+
+        @Override
+        public int getItemCount() {
+            return anime.size();
+        }
+
+        class AnimeViewHolder extends RecyclerView.ViewHolder {
+
+            @BindView(R.id.anime_poster) ImageView animePoster;
+            @BindView(R.id.anime_title) TextView animeName;
+
+            AnimeViewHolder(View itemView) {
+                super(LayoutInflater.from(itemView.getContext()).inflate(R.layout.anime_result, (ViewGroup) itemView, false));
+                ButterKnife.bind(this.itemView);
+            }
+        }
+
     }
 
 }
